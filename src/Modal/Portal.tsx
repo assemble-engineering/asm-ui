de84915af9bd9e87d0791d85196ef38e67ab7841
Ryan Canfield <ryancanfield@me.com>
2022-04-20 14:25:35 -0700

    Add modal and bump version

---
+++ src/Modal/Portal.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+const Portal = ({ children, onClose }: {onClose: () => void; children: React.ReactNode | React.ReactChildren}): JSX.Element => {
+  useEffect(() => {
+    const modalKeyDown = (event: any) => {
+      if (event.key === 'Escape' || event.key === 'Esc') {
+        onClose();
+      }
+    };
+
+    document.body.classList.add('no-scroll');
+    window.addEventListener('keydown', modalKeyDown);
+
+    return () => {
+      document.body.classList.remove('no-scroll');
+      window.removeEventListener('keydown', modalKeyDown);
+    };
+  }, []);
+
+  return createPortal(
+    <>{children}</>,
+    document.body
+  );
+};
+
+export default Portal;
---
+++ src/Modal/index.tsx
@@ -0,0 +1,73 @@
+import React, { ReactNode, useEffect, useRef } from 'react';
+import Portal from './Portal';
+
+export const Modal = ({open, onClose, children, ariaModalLabel, ariaCloseLabel }: {
+  open: boolean;
+  onClose: () => void;
+  children: ReactNode;
+  ariaModalLabel: string;
+  ariaCloseLabel: string;
+}): JSX.Element | null => {
+  const modalRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    modalRef.current?.focus();
+
+    const trapFocus = (e: any) => {
+      const focusableEls = modalRef.current?.querySelectorAll(
+        'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
+      );
+      const firstFocusableEl = focusableEls && focusableEls[0];
+      const lastFocusableEl = focusableEls && focusableEls[focusableEls.length - 1];
+
+      if (e.key === 'Tab') {
+        if (e.shiftKey) {
+          if (document.activeElement === firstFocusableEl) {
+            // @ts-ignore
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } /* tab */ else {
+          if (document.activeElement === lastFocusableEl) {
+            // @ts-ignore
+            firstFocusableEl.focus();
+            e.preventDefault();
+          }
+        }
+      }
+    };
+
+    modalRef.current?.addEventListener('keydown', trapFocus);
+
+    return () => {
+      modalRef.current?.removeEventListener('keydown', trapFocus);
+    };
+  }, [modalRef.current]);
+
+  if (open) {
+    return (
+      <Portal onClose={onClose}>
+        <aside
+          ref={modalRef}
+          className='asm-modal-overlay'
+          role="dialog"
+          aria-label={ariaModalLabel}
+          aria-modal="true"
+          tabIndex={-1}
+        >
+          <div className='asm-modal'>
+            <div className='asm-modal-content'>
+              {children}
+              <button
+                onClick={onClose}
+                className="modal-close"
+                aria-label={ariaCloseLabel}
+              />
+            </div>
+          </div>
+        </aside>
+      </Portal>
+    );
+  }
+  return null;
+};
